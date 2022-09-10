7e46a324f89a239a48fc13b198de949d1c0f6af3
Shehara_r <ranathisarashehara01@gmail.com>
2022-09-10 13:34:05 +0530

    retreiving data

--- src/Pages/ArticlePage.tsx
+++ src/Pages/ArticlePage.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
+  const [article, setArticle] = useState<Article>();
+  const articleID = useParams();
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      await axios
+        .get(`http://localhost:3333/api/articles/find/${articleID.id}`)
+        .then((res) => {
+          setArticle(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+    fetchArticle();
+  }, []);
+
+  return (
+    <div className='pt-20 bg-[#000] text-[#fff] h-[100vh] w-[100vw] overflow-y-scroll scrollbar-none'>
+      <h1>{article?.title}</h1>
+      <img src={article?.imgUrl} alt='' className='' />
+      <p>{article?.desc}</p>
+    </div>
+  );
 };
 
 export default ArticlePage;
