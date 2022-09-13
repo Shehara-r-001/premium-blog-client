74919a85b65bca4ec63c895370e9a34fe87ca9c0
Shehara_r <ranathisarashehara01@gmail.com>
2022-09-13 20:51:55 +0530

    rightbar

--- src/Pages/ArticlePage.tsx
+++ src/Pages/ArticlePage.tsx
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CommentBox from '../components/CommentBox';
+import Rightbar from '../components/Rightbar';
 
 const ArticlePage = () => {
   const [article, setArticle] = useState<Article>();
-  const articleID = useParams();
+  const params = useParams();
 
   useEffect(() => {
     const fetchArticle = async () => {
       await axios
-        .get(`http://localhost:3333/api/articles/find/${articleID.id}`)
+        .get(`http://localhost:3333/api/articles/find/${params.id}`)
         .then((res) => {
           setArticle(res.data);
         })
@@ -19,10 +21,31 @@ const ArticlePage = () => {
   }, []);
 
   return (
-    <div className='pt-20 bg-[#000] text-[#fff] h-[100vh] w-[100vw] overflow-y-scroll scrollbar-none'>
-      <h1>{article?.title}</h1>
-      <img src={article?.imgUrl} alt='' className='' />
-      <p>{article?.desc}</p>
+    <div className='pt-20 bg-[#000] text-[#fff] h-[100vh] w-[100vw] overflow-y-scroll scrollbar-none md:flex md:justify-between md:px-10'>
+      <div className='w-4/5 mx-auto md:w-2/3 md:m-0 md:pl-10 mb-5'>
+        <div className='flex items-center justify-between mb-2'>
+          <h1 className='text-lg font-semibold'>{article?.title}</h1>
+          <p
+            className={`text-sm border p-1 px-2 rounded-md ${
+              article?.access === 'Basic'
+                ? 'border-[#00ffff]'
+                : article?.access === 'Standard'
+                ? 'border-[#0000ff]'
+                : 'border-[#9900cc]'
+            }`}
+          >
+            {article?.access.split('', 100)}
+          </p>
+        </div>
+        <img src={article?.imgUrl} alt='' className='object-cover' />
+        <p className='mt-3 text-sm text-justify'>
+          {article?.desc.split('\n', 100)}
+        </p>
+      </div>
+
+      <CommentBox />
+
+      <Rightbar access={article?.access} />
     </div>
   );
 };
---
+++ src/components/Rightbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useAppSelector } from '../redux/hooks';
+
+type Props = {
+  access: string | undefined;
+};
+
+const Rightbar = ({ access }: Props) => {
+  const user = useAppSelector((state) => state.user.data.email);
+  return (
+    <div className='border border-[#1aff1a] w-4/5 mx-auto md:w-1/4 md:h-[100px] p-3 flex items-start justify-center flex-col rounded-lg md:fixed md:top-20 md:right-5'>
+      <div className='text-base flex flex-wrap items-center justify-between w-full'>
+        <h1 className=''>Signed In as</h1>
+        <h1 className='font-semibold text-[#00ff00] truncate'>{user}</h1>
+      </div>
+      <div className='text-base flex flex-wrap items-center justify-between w-full'>
+        <h1>Subscription Type</h1>
+        <h1
+          className={`font-semibold ${
+            access === 'Basic'
+              ? 'text-[#00ffff]'
+              : access === 'Standard'
+              ? 'text-[#0000ff]'
+              : 'text-[#9900cc]'
+          }`}
+        >
+          {access}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;
