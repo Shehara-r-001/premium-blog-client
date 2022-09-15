6f29dbc33be01d791d8f771ee71fed706aec6d34
Shehara_r <ranathisarashehara01@gmail.com>
2022-09-15 11:56:12 +0530

    fix-border

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Comments = ({ comments }: Props) => {
   return (
-    <div className='border-t border-[#737373] mt-5'>
+    <div className='border-t border-b border-[#737373] mt-5 mb-5'>
       {comments?.map((comment) => (
         <Comment key={comment._id} comment={comment} />
       ))}
